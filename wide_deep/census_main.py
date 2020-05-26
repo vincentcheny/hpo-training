5ed8cf45b322e4942449b45fedb695c7785f7a12
vincent <cy0906@163.com>
2020-05-26 23:23:32 +0800

    add wide_deep

---
+++ wide_deep/census_main.py
@@ -0,0 +1,147 @@
+# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Train DNN on census income dataset."""
+
+import os
+
+from absl import app as absl_app
+from absl import flags
+import tensorflow.compat.v1 as tf
+from . import census_dataset
+from . import wide_deep_run_loop
+import nni
+
+
+def set_defaults(**kwargs):
+  for key, value in kwargs.items():
+    flags.FLAGS.set_default(name=key, value=value)
+
+def define_census_flags():
+  wide_deep_run_loop.define_wide_deep_flags()
+  flags.adopt_module_key_flags(wide_deep_run_loop)
+  set_defaults(data_dir='/uac/rshr/cyliu/bigDataStorage/moo/chen.yu/census_data',
+              model_dir='/uac/rshr/cyliu/bigDataStorage/moo/chen.yu/census_model',
+              train_epochs=40,
+              epochs_between_evals=2,
+              inter_op_parallelism_threads=1,
+              intra_op_parallelism_threads=2,
+              batch_size=40)
+
+
+def build_estimator(model_dir, model_type, model_column_fn, inter_op, intra_op):
+  """Build an estimator appropriate for the given model type."""
+  wide_columns, deep_columns = model_column_fn()
+  hidden_units = [100, 75, 50, 25]
+
+  # Create a tf.estimator.RunConfig to ensure the model is run on CPU, which
+  # trains faster than GPU for this model.
+  my_config = tf.ConfigProto( 
+    inter_op_parallelism_threads=int(params['inter_op_parallelism_threads']),
+    intra_op_parallelism_threads=int(params['intra_op_parallelism_threads']),
+    graph_options=tf.GraphOptions(
+        build_cost_model=int(params['build_cost_model']),
+        infer_shapes=params['infer_shapes'],
+        place_pruned_graph=params['place_pruned_graph'],
+        enable_bfloat16_sendrecv=params['enable_bfloat16_sendrecv'],
+        optimizer_options=tf.OptimizerOptions(
+            do_common_subexpression_elimination=params['do_common_subexpression_elimination'],
+            max_folded_constant_in_bytes=int(params['max_folded_constant']),
+            do_function_inlining=params['do_function_inlining'],
+            global_jit_level=params['global_jit_level'])))
+  run_config = tf.estimator.RunConfig(session_config=my_config)
+  if model_type == 'wide':
+    return tf.estimator.LinearClassifier(
+        model_dir=model_dir,
+        feature_columns=wide_columns,
+        config=run_config)
+  elif model_type == 'deep':
+    return tf.estimator.DNNClassifier(
+        model_dir=model_dir,
+        feature_columns=deep_columns,
+        hidden_units=hidden_units,
+        config=run_config)
+  else:
+    return tf.estimator.DNNLinearCombinedClassifier(
+        model_dir=model_dir,
+        linear_feature_columns=wide_columns,
+        dnn_feature_columns=deep_columns,
+        dnn_hidden_units=hidden_units,
+        config=run_config)
+
+
+def run_census(flags_obj):
+  """Construct all necessary functions and call run_loop.
+
+  Args:
+    flags_obj: Object containing user specified flags.
+  """
+  if flags_obj.download_if_missing:
+    census_dataset.download(flags_obj.data_dir)
+
+  train_file = os.path.join(flags_obj.data_dir, census_dataset.TRAINING_FILE)
+  test_file = os.path.join(flags_obj.data_dir, census_dataset.EVAL_FILE)
+
+  # Train and evaluate the model every `flags.epochs_between_evals` epochs.
+  def train_input_fn():
+    return census_dataset.input_fn(
+        train_file, flags_obj.epochs_between_evals, True, flags_obj.batch_size)
+
+  def eval_input_fn():
+    return census_dataset.input_fn(test_file, 1, False, flags_obj.batch_size)
+
+  tensors_to_log = {
+      'average_loss': '{loss_prefix}head/truediv',
+      'loss': '{loss_prefix}head/weighted_loss/Sum'
+  }
+
+  wide_deep_run_loop.run_loop(
+      name="Census Income", train_input_fn=train_input_fn,
+      eval_input_fn=eval_input_fn,
+      model_column_fn=census_dataset.build_model_columns,
+      build_estimator_fn=build_estimator,
+      flags_obj=flags_obj,
+      tensors_to_log=tensors_to_log,
+      early_stop=True)
+
+
+def main(_):
+  run_census(flags.FLAGS)
+
+def get_default_params():
+  return {
+    "BATCH_SIZE":32,
+    "LEARNING_RATE":1e-4,
+    "NUM_EPOCHS":30,
+    "inter_op_parallelism_threads":1,
+    "intra_op_parallelism_threads":2,
+    "max_folded_constant":6,
+    "build_cost_model":4,
+    "do_common_subexpression_elimination":1,
+    "do_function_inlining":1,
+    "global_jit_level":1,
+    "infer_shapes":1,
+    "place_pruned_graph":1,
+    "enable_bfloat16_sendrecv":1
+  }
+
+if __name__ == '__main__':
+  tf.logging.set_verbosity(tf.logging.INFO)
+  define_census_flags()
+  params = get_default_params()
+  received_params = nni.get_next_parameter()
+  params.update(received_params)
+  flags.FLAGS.train_epochs = params["NUM_EPOCHS"]
+  flags.FLAGS.batch_size = params['BATCH_SIZE']
+  absl_app.run(main)
---
+++ wide_deep/wide_deep_run_loop.py
@@ -0,0 +1,132 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Core run logic for TensorFlow Wide & Deep Tutorial using tf.estimator API."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import shutil
+
+from absl import app as absl_app
+from absl import flags
+import tensorflow.compat.v1 as tf
+
+import nni
+
+LOSS_PREFIX = {'wide': 'linear/', 'deep': 'dnn/'}
+
+
+def define_wide_deep_flags():
+  """Add supervised learning flags, as well as wide-deep model type."""
+  flags.DEFINE_boolean(
+        name="clean", default=False,
+        help=help_wrap("If set, model_dir will be removed if it exists."))
+  flags.DEFINE_integer(
+        name="train_epochs", short_name="te", default=1,
+        help=help_wrap("The number of epochs used to train."))
+  flags.DEFINE_integer(
+        name="epochs_between_evals", short_name="ebe", default=1,
+        help=help_wrap("The number of training epochs to run between "
+                       "evaluations."))
+  flags.DEFINE_float(
+        name="stop_threshold", short_name="st",
+        default=None,
+        help=help_wrap("If passed, training will stop at the earlier of "
+                       "train_epochs and when the evaluation metric is  "
+                       "greater than or equal to stop_threshold."))
+  flags.DEFINE_list(
+        name="hooks", short_name="hk", default="LoggingTensorHook",
+        help=help_wrap(
+            u"A list of (case insensitive) strings to specify the names of "
+            u"training hooks. Example: `--hooks ProfilerHook,"
+            u"ExamplesPerSecondHook`\n See hooks_helper "
+            u"for details.")
+  )
+  flags.DEFINE_string(
+        name="export_dir", short_name="ed", default=None,
+        help=help_wrap("If set, a SavedModel serialization of the model will "
+                       "be exported to this directory at the end of training. "
+                       "See the README for more details and relevant links.")
+  )
+
+  flags.DEFINE_enum(
+      name="model_type", short_name="mt", default="wide_deep",
+      enum_values=['wide', 'deep', 'wide_deep'],
+      help="Select model topology.")
+  flags.DEFINE_boolean(
+      name="download_if_missing", default=True, 
+          "Download data to data_dir if it is not already present.")
+
+
+def export_model(model, model_type, export_dir, model_column_fn):
+  """Export to SavedModel format.
+
+  Args:
+    model: Estimator object
+    model_type: string indicating model type. "wide", "deep" or "wide_deep"
+    export_dir: directory to export the model.
+    model_column_fn: Function to generate model feature columns.
+  """
+  wide_columns, deep_columns = model_column_fn()
+  if model_type == 'wide':
+    columns = wide_columns
+  elif model_type == 'deep':
+    columns = deep_columns
+  else:
+    columns = wide_columns + deep_columns
+  feature_spec = tf.feature_column.make_parse_example_spec(columns)
+  example_input_fn = (
+      tf.estimator.export.build_parsing_serving_input_receiver_fn(feature_spec))
+  model.export_savedmodel(export_dir, example_input_fn,
+                          strip_default_attrs=True)
+
+
+def run_loop(name, train_input_fn, eval_input_fn, model_column_fn,
+             build_estimator_fn, flags_obj, tensors_to_log, early_stop=False):
+  """Define training loop."""
+  model = build_estimator_fn(
+      model_dir=flags_obj.model_dir, model_type=flags_obj.model_type,
+      model_column_fn=model_column_fn,
+      inter_op=flags_obj.inter_op_parallelism_threads,
+      intra_op=flags_obj.intra_op_parallelism_threads)
+
+
+  loss_prefix = LOSS_PREFIX.get(flags_obj.model_type, '')
+  tensors_to_log = {k: v.format(loss_prefix=loss_prefix)
+                    for k, v in tensors_to_log.items()}
+
+  # Train and evaluate the model every `flags.epochs_between_evals` epochs.
+  for n in range(flags_obj.train_epochs // flags_obj.epochs_between_evals):
+    model.train(input_fn=train_input_fn, hooks=train_hooks)
+
+    results = model.evaluate(input_fn=eval_input_fn)
+
+    # Display evaluation metrics
+    tf.logging.info('Results at epoch %d / %d',
+                    (n + 1) * flags_obj.epochs_between_evals,
+                    flags_obj.train_epochs)
+    tf.logging.info('-' * 60)
+
+    for key in sorted(results):
+      tf.logging.info('%s: %s' % (key, results[key]))
+
+    nni.report_intermediate_result(results['accuracy'])
+
+    if early_stop:
+      break
+  
+  nni.report_final_result(results['accuracy'])
