491e927dd90bebfc657c17547bdb876c39cf6f09
vincent <cy0906@163.com>
2020-05-28 00:22:45 +0800

    update bert

--- bert/bert.py
+++ bert/bert.py
@@ -235,5 +235,5 @@ for i in tqdm(range(0,len(preds),2)):
 nni.report_final_result(accuracy_score(np.array(results), test_labels))
 print(f1_score(np.array(results), test_labels))
 # Delete the checkpoint and summary for next trial
-if os.path.exists(FLAGS.model_dir):
-    shutil.rmtree(FLAGS.model_dir)
+if os.path.exists(OUTPUT_DIR):
+    shutil.rmtree(OUTPUT_DIR)
